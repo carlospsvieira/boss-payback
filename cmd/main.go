1a5c5dcb588410ce0c936f9cfb42cf0643e6ff75
carlos <carlepsvieira@gmail.com>
2024-01-02 16:00:04 -0300

    initialize db

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"render2/internal/db"
+	"render2/internal/db/models"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,8 +12,21 @@ import (
 func main() {
 	app := fiber.New()
 
+	database, err := db.InitializeDatabase()
+	if err != nil {
+		panic("Failed to initialize database!")
+	}
+
+	// Example: Fetching a user from the database by ID (just an illustration)
+	var user models.User
+	result := database.First(&user, 1) // Assuming user ID 1 exists
+	if result.Error != nil {
+		panic("Failed to fetch user!")
+	}
+	fmt.Printf("Fetched User: %+v\n", user)
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString(user.Username)
 	})
 
 	log.Fatal(app.Listen(":3000"))
